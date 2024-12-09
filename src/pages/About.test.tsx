ba68ee5f78f9011c556b1fb292f9eb7bc3d3b7a0
mrvin100 <vincentyoumssi@gmail.com>
2024-12-09 11:56:38 +0100

    ajout de modifs pour setup vitest correctement

--- src/pages/About.test.tsx
+++ src/pages/About.test.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom' // Extends expect with custom matchers like toBeInTheDocument
 import About from './About'
 
 describe('About', () => {
